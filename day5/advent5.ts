2fa6c1b45267c4588a0a54d1a23b22f5c80e88a4
AnandPap <anandpap@live.com>
2025-12-06 02:48:20 +0100

    Second two gold star solution that is much cleaner and which uses current maximum covered while looping over array and unionizing ranges

--- day5/advent5.ts
+++ day5/advent5.ts
@@ -1,34 +1,18 @@
 import { input } from "./input5";
 
-function sortAscendingByEndOfRange(ranges: number[][]) {
-  ranges.sort((a, b) => a[1] - b[1] || a[0] - b[0]);
-}
-
 function f() {
   const splitInput = input.split("\n\n");
   const ranges = splitInput[0].split("\n").map((range) => range.split("-").map((border) => Number(border)));
 
-  sortAscendingByEndOfRange(ranges);
+  ranges.sort((a, b) => a[0] - b[0]);
 
-  // Case when second range envelops the prior, add their bottom difference as separate range
-  const newRanges = [];
-  for (let i = 1; i < ranges.length; i++) {
-    if (ranges[i][0] < ranges[i - 1][0]) {
-      newRanges.push([ranges[i][0], ranges[i - 1][0] - 1]);
-    }
-  }
-  ranges.push(...newRanges);
-
-  sortAscendingByEndOfRange(ranges);
-
-  // Cut beginning of ranges to start after prior ends
-  for (let i = 1; i < ranges.length; i++) {
-    if (ranges[i][0] <= ranges[i - 1][1]) ranges[i][0] = ranges[i - 1][1] + 1;
-  }
-
+  let current = 0;
   let count = 0;
   for (const [begin, end] of ranges) {
-    count += end - begin + 1;
+    if (current > end) continue;
+    if (current < begin) current = begin;
+    count += end - current + 1;
+    current = end + 1;
   }
 
   console.log(count);
